2b634d404f88310fa4fc60a668a57c5482c77265
Nikita <inekit392000.ii@gmail.com>
2019-04-23 13:46:38 +0300

    myscndsvr

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express=require('express')
 const app=express()
+const pool=require('./config/config')
 
 app.route('/').get((request,response) => {response.send('Hi')})
 
-app.listen(8080, ()=> {
+app.route('/api/t/:group').get((request,response) => {
+    let ngroup=request.params['group']
+    response.send(`Hi ${ngroup}`)})
+
+app.route('/api/students').get((request,response)=>{
+pool.query('Select * from students', (err,result,fields)=>{
+if (err) throw err
+response.send(result);
+})
+})
+
+app.listen(8081, ()=> {
     console.log('server started')
 })
