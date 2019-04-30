9aa6775840bdf15ab68d8a5a7c980c34c0f27c85
xzdv <dnf>
2019-04-30 14:18:53 +0300

    v

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express=require('express')
 const app=express()
+const bodyParser=require('express')
 const pool=require('./config/config')
+
+app.use(bodyParser.urlencoded({ extended:false}))
+app.use(bodyParser.json())
+
+app.route('/api/students').post((request,response)=>{
+    let{name}=request.body
+
+    pool.query('Insert into students (surname) values (?)',
+    [name],(err,res)=>
+    {
+        console.log(err);
+        res.send(res);
+     })
+})
 
 app.route('/').get((request,response) => {response.send('Hi')})
 
